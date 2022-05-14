7781118b1338b571ee13dae7bd2f9b3dd87abbbe
Jethro Dean <jethroldean@gmail.com>
2022-05-14 14:34:43 +1000

    completed

--- src/App.js
+++ src/App.js
@@ -1,70 +1,151 @@
 import React from "react"
 import "./App.scss"
 
-export default function App() {
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+const operators = ["/", "*", "-", "+"]
+const ids = {
+    7: "seven", 
+    8: "eight", 
+    9: "nine", 
+    4: "four", 
+    5: "five", 
+    6: "six", 
+    1: "one", 
+    2: "two", 
+    3: "three", 
+    0: "zero",
+    "/": "divide", 
+    "*": "multiply", 
+    "-": "subtract", 
+    "+": "add"
+}
 
-    const [input, setInput] = React.useState("")
-    const [output, setOutput] = React.useState(0)
-
-    const display = (text) => {
-        let lastText = input[input.length-1]
-
-        setInput(prevState => prevState + text)
-
-        if (lastText == "=") {
-            if (/[0-9.]/.test(text)) {
-                setInput(text)
-            } else {
-                setInput(output + text)
-            }
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            lastPressed: undefined,
+            calc: "0",
+            operation: undefined
         }
     }
 
-    const getAnswer = () => {
-        setOutput(eval(input))
-        setInput((prevInput) => prevInput + "=")
+    handleClick = (e) => {
+        const { calc, lastPressed } = this.state
+        const { innerText } = e.target
+
+        switch(innerText) {
+            case "AC": {
+                this.setState({
+                    calc: "0"
+                })
+                break;
+            }
+            case "=": {
+                const calculation = eval(calc)
+                this.setState({
+                    calc: calculation
+                })
+                break;
+            }
+            case ".": {
+                const calcSplit = calc.split(/[\+\-\*\/]/)
+                const last = calcSplit.slice(-1)[0]
+
+                if (!last.includes(".")) {
+                    this.setState({
+                        calc: calc + "."
+                    })
+                }
+
+                break;
+            }
+            default: {
+                let e = undefined
+                if (operators.includes(innerText)) {
+                    if (operators.includes(lastPressed) && innerText !== "-") {
+                        const lastNumberIndex = calc.split('').reverse()
+                            .findIndex(char => char !== " " && numbers.includes(+char))
+                        e = calc.slice(0, calc.length - lastNumberIndex) + ` ${innerText} `
+                    } else {
+                        e = `${calc} ${innerText} `
+                    }
+                } else {
+                    e = (calc === "0") ? innerText : (calc + innerText)
+                }
+                this.setState({
+                    calc: e
+                })
+            }
+        }
+        this.setState({
+            lastPressed: innerText
+        })
     }
 
-    const clear = () => {
-        setInput("")
-        setOutput(0)
-    }
+    render() {
 
-    return (
-        <div className="container">
-            <div className="grid">
-                <div className="display">
-                    <input 
-                        className="input-display" 
-                        placeholder="0" 
-                        value={input} 
-                        disabled 
-                    />
-                    <input 
-                        id="display" 
-                        className="output-display" 
-                        value={output}
-                        disabled
-                    />
+        const { calc } = this.state
+
+        return (
+            <div className="container">
+                <div id="display" className="display">
+                    {calc}
                 </div>
-                <div onClick={clear} className="button double-length" id="clear">AC</div>
-                <div onClick={() => display("/")} className="button operators" id="divide">/</div>
-                <div onClick={() => display("*")} className="button operators" id="multiply">*</div>
-                <div onClick={() => display("7")} className="button" id="seven">7</div>
-                <div onClick={() => display("8")} className="button" id="eight">8</div>
-                <div onClick={() => display("9")} className="button" id="nine">9</div>
-                <div onClick={() => display("-")} className="button operators" id="subtract">-</div>
-                <div onClick={() => display("4")} className="button" id="four">4</div>
-                <div onClick={() => display("5")} className="button" id="five">5</div>
-                <div onClick={() => display("6")} className="button" id="six">6</div>
-                <div onClick={() => display("+")} className="button operators" id="add">+</div>
-                <div onClick={() => display("1")} className="button" id="one">1</div>
-                <div onClick={() => display("2")} className="button" id="two">2</div>
-                <div onClick={() => display("3")} className="button" id="three">3</div>
-                <div onClick={getAnswer} className="button double-height" id="equals">=</div>
-                <div onClick={() => display("0")} className="button double-length" id="zero">0</div>
-                <div onClick={() => display(".")} className="button" id="decimal">.</div>
+
+                <div className="number-container">
+                    <button 
+                        id="clear" 
+                        className="purple clear triple-length bold" 
+                        onClick={this.handleClick}
+                    >
+                        AC
+                    </button>
+
+                    {numbers.map(number => (
+                        <button 
+                            id={ids[number]}
+                            className={`light-grey ${number === 0 && 'double-length'}`} 
+                            key={number} 
+                            onClick={this.handleClick}
+                        >
+                            {number}
+                        </button>
+                    ))}
+
+                    <button 
+                        id="decimal" 
+                        className="light-grey" 
+                        onClick={this.handleClick}
+                    >
+                        .
+                    </button>
+
+                </div>
+
+                <div className="operator-container">
+                    {operators.map(operator => (
+                        <button 
+                            id={ids[operator]}
+                            className='dark-grey' 
+                            key={operator} 
+                            onClick={this.handleClick}
+                        >
+                            {operator}
+                        </button>
+                    ))}
+
+                    <button 
+                        id="equals" 
+                        className="purple bold" 
+                        onClick={this.handleClick}
+                    >
+                        =
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default App 
